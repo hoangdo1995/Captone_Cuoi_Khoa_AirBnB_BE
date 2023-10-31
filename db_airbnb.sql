f2550f80a6612b85f3ca21c424e4ceb1209b28a6
dohoang790@gmail.com <dohoang790@gmail.com>
2023-11-01 05:39:26 +0700

    final

--- db_airbnb.sql
+++ db_airbnb.sql
@@ -19,7 +19,7 @@ CREATE TABLE `comment` (
   KEY `room_id` (`room_id`),
   CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `country` (
   `id` mediumint NOT NULL AUTO_INCREMENT,
@@ -38,13 +38,13 @@ CREATE TABLE `location` (
   KEY `country` (`country`),
   CONSTRAINT `location_ibfk_1` FOREIGN KEY (`province`) REFERENCES `province` (`id`),
   CONSTRAINT `location_ibfk_2` FOREIGN KEY (`country`) REFERENCES `country` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `province` (
   `id` int NOT NULL AUTO_INCREMENT,
   `province_name` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `role` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -77,7 +77,7 @@ CREATE TABLE `room` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `room_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `location` (`id`),
   CONSTRAINT `room_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `room_book` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -91,7 +91,7 @@ CREATE TABLE `room_book` (
   KEY `room_id` (`room_id`),
   CONSTRAINT `room_book_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `room_book_ibfk_2` FOREIGN KEY (`room_id`) REFERENCES `room` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `user` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -106,9 +106,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   KEY `role` (`role`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
+INSERT INTO `comment` (`id`, `user_id`, `date`, `content`, `rate_comment`, `room_id`) VALUES
+(3, 21, '2023-10-29', 'Thân thiện với khách hàng!', 5, 10);
+INSERT INTO `comment` (`id`, `user_id`, `date`, `content`, `rate_comment`, `room_id`) VALUES
+(4, 12, '2023-10-30', 'Phục vụ tốt', 5, 10);
+INSERT INTO `comment` (`id`, `user_id`, `date`, `content`, `rate_comment`, `room_id`) VALUES
+(7, 20, '2023-10-31', 'Phòng đẹp, Nhân viên chu đáo!', 4, 10);
+INSERT INTO `comment` (`id`, `user_id`, `date`, `content`, `rate_comment`, `room_id`) VALUES
+(8, 20, '2023-10-31', 'Phòng đẹp, Nhân viên chu đáo!', 4, 13);
 
 INSERT INTO `country` (`id`, `country_name`) VALUES
 (1, 'Việt Nam');
@@ -124,18 +131,28 @@ INSERT INTO `location` (`id`, `location_name`, `province`, `country`, `image`) V
 INSERT INTO `location` (`id`, `location_name`, `province`, `country`, `image`) VALUES
 (2, 'Xã Diên Khánh', 2, 1, 'image');
 INSERT INTO `location` (`id`, `location_name`, `province`, `country`, `image`) VALUES
-(8, 'string', 1, 1, 'string');
+(8, 'Quận 1', 1, 1, 'string');
 INSERT INTO `location` (`id`, `location_name`, `province`, `country`, `image`) VALUES
-(9, 'string a', 1, 1, 'string'),
-(10, 'Room', 1, 1, 'string'),
-(11, 'Xã Bình Điền', 1, 1, 'string'),
-(12, 'Xã Tân Hiệp', 1, 1, 'string');
+(9, 'Quận 7', 1, 1, 'string'),
+(10, 'Huyện Hóc Môn', 1, 1, 'string'),
+(11, 'Quận Bình Thạnh', 1, 1, 'string'),
+(12, 'Quận Tân Bình', 1, 1, 'string'),
+(13, 'Huyện Củ Chi', 1, 1, 'string'),
+(14, 'Quận Bình Chánh', 1, 1, 'string'),
+(15, 'Thành phố Phúc Châu', 3, 3, 'string'),
+(16, 'Thành phố Lan Châu', 4, 3, ''),
+(17, 'Thạch Gia Trang', 6, 3, 'string');
 
 INSERT INTO `province` (`id`, `province_name`) VALUES
 (1, 'Hồ Chí Minh');
 INSERT INTO `province` (`id`, `province_name`) VALUES
 (2, 'Nha Trang');
-
+INSERT INTO `province` (`id`, `province_name`) VALUES
+(3, 'Phúc Kiến');
+INSERT INTO `province` (`id`, `province_name`) VALUES
+(4, 'Cam Túc'),
+(5, 'Quảng Đông'),
+(6, 'Hà Bắc');
 
 INSERT INTO `role` (`id`, `role_name`, `permission`) VALUES
 (1, 'admin', NULL);
@@ -145,11 +162,17 @@ INSERT INTO `role` (`id`, `role_name`, `permission`) VALUES
 (3, 'leaser', NULL);
 
 INSERT INTO `room` (`id`, `room_name`, `living_room`, `bedroom`, `bed`, `bathroom`, `description`, `cost`, `washing_machine`, `iron`, `televition`, `air_condition`, `wifi`, `parking`, `kitchen`, `image`, `location_id`, `user_id`) VALUES
-(10, 'string', 0, 0, 0, 0, 'string', 0, 1, 1, 1, 1, 1, 1, 1, 'string', 1, 12);
+(10, 'Trà My Home Stay', 1, 1, 2, 1, 'string', 0, 1, 1, 1, 1, 1, 1, 1, 'string', 1, 12);
 INSERT INTO `room` (`id`, `room_name`, `living_room`, `bedroom`, `bed`, `bathroom`, `description`, `cost`, `washing_machine`, `iron`, `televition`, `air_condition`, `wifi`, `parking`, `kitchen`, `image`, `location_id`, `user_id`) VALUES
 (13, 'Hotel 24H', 1, 1, 1, 1, 'string', 0, 1, 1, 1, 1, 1, 1, 1, 'string', 11, 12);
 INSERT INTO `room` (`id`, `room_name`, `living_room`, `bedroom`, `bed`, `bathroom`, `description`, `cost`, `washing_machine`, `iron`, `televition`, `air_condition`, `wifi`, `parking`, `kitchen`, `image`, `location_id`, `user_id`) VALUES
-(14, 'Điền Phong', 0, 0, 0, 0, 'string', 0, 1, 1, 1, 1, 1, 1, 1, 'string', 11, 12);
+(14, 'Điền Phong', 0, 1, 1, 1, 'string', 0, 1, 1, 1, 1, 1, 1, 1, 'string', 11, 12);
+INSERT INTO `room` (`id`, `room_name`, `living_room`, `bedroom`, `bed`, `bathroom`, `description`, `cost`, `washing_machine`, `iron`, `televition`, `air_condition`, `wifi`, `parking`, `kitchen`, `image`, `location_id`, `user_id`) VALUES
+(16, 'Hoàng Hà Hotel', 1, 1, 1, 1, '\'\'', 12, 1, 1, 1, 1, 1, 1, 1, NULL, 10, 12),
+(17, 'Hoàng Hà Hotel', 1, 1, 1, 1, '\'\'', 12, 1, 1, 1, 1, 1, 1, 1, NULL, 10, 12),
+(18, 'Phòng trọ cao cấp', 1, 1, 1, 1, 'Phòng trọ cao cấp Củ chi', 12, 1, 1, 1, 1, 1, 1, 1, '1698697972619phongtro.jpg', 10, 12),
+(19, 'Trần Tiến Hotel', 1, 1, 2, 1, '\'\'', 1, 0, 0, 0, 0, 0, 0, 0, '1698762880430hotel.jpg', 12, 19),
+(20, 'White Palace Hotel', 1, 1, 2, 2, '\'Khách sạn chất lượng 5 sao\'', 100, 1, 1, 1, 1, 1, 1, 1, '1698791047726hotel.jpg', 8, 12);
 
 INSERT INTO `room_book` (`id`, `room_id`, `start_day`, `end_day`, `guest_count`, `user_id`) VALUES
 (45, 10, '2023-10-17', '2023-10-17', 6, 16);
@@ -158,17 +181,21 @@ INSERT INTO `room_book` (`id`, `room_id`, `start_day`, `end_day`, `guest_count`,
 INSERT INTO `room_book` (`id`, `room_id`, `start_day`, `end_day`, `guest_count`, `user_id`) VALUES
 (47, 10, '2023-10-15', '2023-10-15', 0, 20);
 INSERT INTO `room_book` (`id`, `room_id`, `start_day`, `end_day`, `guest_count`, `user_id`) VALUES
-(48, 10, '2023-10-19', '2023-10-19', 0, 16);
+(48, 10, '2023-10-19', '2023-10-19', 0, 16),
+(49, 13, '2023-10-29', '2023-10-29', 2, 21);
 
 INSERT INTO `user` (`id`, `full_name`, `email`, `password`, `birth_day`, `phone`, `role`, `gender`, `avatar`) VALUES
-(12, 'admin', 'admin', '$2b$10$QMvipL91kcd1FWQkxR/iEOaQn8hotDKT0wG31nxIvuqTzvB4SGYzy', NULL, NULL, 1, 1, '1697292426753h___372.jpg');
+(12, 'admin', 'admin', '$2b$10$QMvipL91kcd1FWQkxR/iEOaQn8hotDKT0wG31nxIvuqTzvB4SGYzy', '1980-09-11', '0126798043', 1, 1, '1697292426753h___372.jpg');
 INSERT INTO `user` (`id`, `full_name`, `email`, `password`, `birth_day`, `phone`, `role`, `gender`, `avatar`) VALUES
-(15, 'string', 'a', '$2b$10$Y1MRTq8skI8IWl1mZFZ/0O7BAI0UREJy6aNI5SDR.jPLfpNf9MLMW', NULL, NULL, 3, 1, NULL);
+(15, 'Đình Toàn', 'dinhtoanlam@gmail.com', '$2b$10$Y1MRTq8skI8IWl1mZFZ/0O7BAI0UREJy6aNI5SDR.jPLfpNf9MLMW', '1997-10-01', '0843535343', 3, 1, NULL);
 INSERT INTO `user` (`id`, `full_name`, `email`, `password`, `birth_day`, `phone`, `role`, `gender`, `avatar`) VALUES
-(16, 'string', 'string', '$2b$10$6NsTbReHa5boL3fXlMQRWe2WpMWc6OEwZx3./TDDXckbyE2EqLYua', NULL, NULL, 2, 1, NULL);
+(16, 'Xuân Mạnh', 'xuanmanh69@gmail.com', '$2b$10$6NsTbReHa5boL3fXlMQRWe2WpMWc6OEwZx3./TDDXckbyE2EqLYua', '1990-10-02', '0967456343', 2, 1, NULL);
 INSERT INTO `user` (`id`, `full_name`, `email`, `password`, `birth_day`, `phone`, `role`, `gender`, `avatar`) VALUES
-(19, 'string', 'hoang', '$2b$10$90oXJQcacS6GH1Ap1viuWu.zrx7evPkX/OOBuo6vVDidO9adtvhlu', NULL, NULL, 3, 1, NULL),
-(20, 'string', 'user2', '$2b$10$GcrSeER4.0D0w5vXZ525MerK4TBZ6EeG/YsP0RzUQAqhMfbO4x1TK', NULL, NULL, 2, 1, NULL);
+(19, 'Tuấn Dũng', 'tuandung@gmail.com', '$2b$10$90oXJQcacS6GH1Ap1viuWu.zrx7evPkX/OOBuo6vVDidO9adtvhlu', '1981-01-01', '0568763243', 3, 1, NULL),
+(20, 'Hoàng Anh', 'hoanganh123@gmail.com', '$2b$10$GcrSeER4.0D0w5vXZ525MerK4TBZ6EeG/YsP0RzUQAqhMfbO4x1TK', '1978-06-03', '0732132144', 2, 1, NULL),
+(21, 'test demo', 'test@gmail.com', '$2b$10$DGRah1N4vrEPOXhLYOygt.5pdYFSMRVL2jCb8JSszhSd0Qq/t9Ioe', '2000-10-29', '0313133243', 2, 1, '1698592202122h___372.jpg'),
+(22, 'Hoàng', 'test123@gmail.com', '$2b$10$sDgAhDJ94B/bClvz7PLyXumuHzOCqy6qOMlEUtwk6bEGRl9euTm8.', '2001-10-30', '0962345678', 2, 1, '1698674050227h___372.jpg'),
+(24, 'Hoàng Đỗ', 'testdemo@gmail.com', '$2b$10$1ZcASMSD.YmWa2GV0TsjfepuDhK8De42pKqGpxN9TAVYg.FoKPCii', '2001-10-31', '098763453', 2, 1, '1698790242813h___372.jpg');
 
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
